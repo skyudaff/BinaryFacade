51c045bd5d2671bc9b22c20b73c237cddedf9d95
Vladimir Razuvaev <skyudaff@mail.ru>
2023-07-26 14:11:35 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        //демонстрация
+        System.out.println(bins.sum("10","11"));
+        System.out.println(bins.mult("10","11"));
+    }
+}
